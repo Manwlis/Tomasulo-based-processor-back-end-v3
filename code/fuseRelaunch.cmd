410f4c3ec5e8b92b51ae937806af1e907aedfb7a
manolis <manolios1996@gmail.com>
2018-12-18 16:26:41 +0200

    3.10.0
    anafora
        optimization perigrafh kai tests
    
    tests
        optimization sto RAW

--- code/fuseRelaunch.cmd
+++ code/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/arxitektonikh1/HRY415-project-3/code/exception_back_end_test_isim_beh.exe" -prj "C:/arxitektonikh1/HRY415-project-3/code/exception_back_end_test_beh.prj" "work.exception_back_end_test" 
+-intstyle "ise" -incremental -o "C:/arxitektonikh1/HRY415-project-3/code/tuxaies_entoles_me_eksartiseis_isim_beh.exe" -prj "C:/arxitektonikh1/HRY415-project-3/code/tuxaies_entoles_me_eksartiseis_beh.prj" "work.tuxaies_entoles_me_eksartiseis" 
